afba981fbccc6db66d715d4eda00506b355dd318
junwoo0823 <junwoo0823@gmail.com>
2022-07-21 16:57:46 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+// 로딩 화면
+$(document).ready(function () {
+  setTimeout(() => {
+    $("#loading").fadeOut(600);
+  }, 6000);
+});
+
 // 로딩 카운트다운
 let count = 5;
 let timer = setInterval(function () {
@@ -8,8 +15,8 @@ let timer = setInterval(function () {
   }
 }, 1000);
 
+// 로딩 화면 타이핑
 setTimeout(() => {
-// 타이핑 효과
   let typed = new Typed('.loading-text', {
     strings: ["안녕하세요.", "웹퍼블리셔 <strong>김준우</strong>입니다."],
     typeSpeed: 80,
@@ -18,53 +25,112 @@ setTimeout(() => {
   });
 }, 500);
 
+// 로딩 종료 후 인트로
 setTimeout(() => {
   document.querySelector('.intro-box').style.transform = "scale(1)";
 }, 6300);
 
+// 화면 드래그 이동
+$("#project-icon, #skill-icon, .screen, #intro").draggable({
+  containment: "window"
+});
+
+// 프로젝트 관련 버튼
+$("#project-icon").click(function () {
+  $("#project-btn").show().css('display', 'flex');
+  $("#project-screen").toggle();
+});
+
+$("#project-btn, #project-min").click(function () {
+  $("#project-screen").toggle();
+});
+
+$("#project-close").click(function () {
+  $("#project-screen, #project-btn, .sample-box").hide();
+});
+
 // 프로젝트 화면
 const show_layer = (divId) => {
-  let bkBox = document.getElementById('burgerKingBox');
-  let mnBox = document.getElementById('melonBox');
-  let qzBox = document.getElementById('quizCatBox');
-  let unBox = document.getElementById('undefinedBox');
+  let bkBox = document.getElementById('burger-king-box');
+  let mnBox = document.getElementById('melon-box');
+  let qzBox = document.getElementById('quiz-cat-box');
+  let pbBox = document.getElementById('paris-baguette-box');
+  // let un-box = document.getElementById('undefined-box');
 
   bkBox.style.display = "none";
   mnBox.style.display = "none";
   qzBox.style.display = "none";
-  unBox.style.display = "none";
+  pbBox.style.display = "none";
+  // un-box.style.display = "none";
 
   switch (divId) {
     case '1': bkBox.style.display = "block"; break;
     case '2': mnBox.style.display = "block"; break;
     case '3': qzBox.style.display = "block"; break;
-    case '4': unBox.style.display = "block"; break;
+    case '4': pbBox.style.display = "block"; break;
+    // case '999': un-box.style.display = "block"; break;
   }
 }
 
-// 실시간 시간 변경
-const currentTime = () => {
-  let today = new Date();
-  let hours = today.getHours();
-  let minutes = today.getMinutes();
-  let time = document.getElementById('f-time');
+// 프로젝트 화면 클릭
+$(".sample-box").click(function () {
+  $(this).stop().fadeOut(200);
+});
 
-  if (minutes < 10) {
-    minutes = '0' + minutes;
+// 스킬 관련 버튼
+$("#skill-icon").click(function () {
+  $("#skill-btn").show().css('display', 'flex');
+  $("#skill-screen").toggle();
+});
+
+$("#skill-btn, #skill-min").click(function () {
+  $("#skill-screen").toggle();
+});
+
+$("#skill-close").click(function () {
+  $("#skill-screen, #skill-btn").hide();
+  $('.skill-detail').removeClass('active').css('display', 'none');
+  $('.skill-detail').css('background', '');
+  $('.detail-content, .detail-back').css('opacity', '0');
+  $('.skill-list>ul, .skill-title').css('opacity', '1');
+});
+
+// 스킬 이미지 클릭
+$('.skill-img').click(function () {
+  let tabId = $(this).attr('data-tab');
+  $("#" + tabId).addClass('active').css('display', 'flex');
+  setTimeout(function () {
+    $('.skill-detail').css('background', 'white');
+    $('.detail-content, .detail-back').css('opacity', '1');
+    $('.skill-list>ul, .skill-title').css('opacity', '0')
+  }, 500);
+});
+
+// 스킬 뒤로가기 클릭
+$('.detail-back').click(function () {
+  $('.skill-detail').fadeOut(500, function () {
+    setTimeout(function () {
+      $('.skill-detail').removeClass('active').css('display', 'none');
+      $('.skill-detail').css('background', '');
+      $('.detail-content, .detail-back').css('opacity', '0');
+    }, 300);
+  });
+  setTimeout(function () {
+    $('.skill-list>ul, .skill-title').css('opacity', '1');
+  }, 300);
+});
+
+// 시작 버튼 토글
+$("#start, .start-main>li").click(function () {
+  $("#start-menu").toggle();
+});
+
+// 시작 메뉴 외 클릭
+$("html").click((e) => {
+  if ($(e.target).parents("footer").length < 1) {
+    $("#start-menu").hide();
   }
-  if (hours < 12) {
-    time.innerHTML = '오전 ' + hours + ':' + minutes;
-  } else {
-    if (hours == 12) {
-      time.innerHTML = '오후 ' + hours + ':' + minutes;
-    } else if (hours > 12) {
-      hours = hours - 12;
-      time.innerHTML = '오후 ' + hours + ':' + minutes;
-    }
-  }
-  setTimeout('currentTime()', 1000);
-}
-window.onload = currentTime;
+});
 
 // 종료 버튼 그룹
 let offBtn = document.getElementById('portfolio-off');
@@ -100,95 +166,32 @@ cancelBtn.addEventListener("click", function () {
   exit.style.display = "none";
 });
 
-// jQuery
-$(document).ready(function () {
-  // 로딩 화면
-  setTimeout(() => {
-    $("#loading").fadeOut(600);
-  }, 6000);
-
-  // 시작 버튼 토글
-  $("#start, .start-main>li").click(function () {
-    $("#start-menu").toggle();
-  });
-
-  // 시작 메뉴 외 클릭
-  $("html").click((e) => {
-    if ($(e.target).parents("footer").length < 1) {
-      $("#start-menu").hide();
-    }
-  });
-
-  // 아이콘 클릭
-  $("#skill-icon").click(function () {
-    $("#skill-btn").show().css('display', 'flex');
-    $("#skill-screen").toggle();
-  });
-
-  $("#project-icon").click(function () {
-    $("#project-btn").show().css('display', 'flex');
-    $("#project-screen").toggle();
-  });
-
-  $("#skill-btn, #skill-min").click(function () {
-    $("#skill-screen").toggle();
-  });
-
-  $("#project-btn, #project-min").click(function () {
-    $("#project-screen").toggle();
-  });
-
-  $("#skill-close").click(function () {
-    $("#skill-screen, #skill-btn").hide();
-    $('.skill-detail').removeClass('active').css('display', 'none');
-    $('.skill-detail').css('background', '');
-    $('.detail-content, .detail-back').css('opacity', '0');
-    $('.skill-list>ul, .skill-title').css('opacity', '1');
-  });
-
-  $("#project-close").click(function () {
-    $("#project-screen, #project-btn, .sample-box").hide();
-  });
-
-  // 스킬 이미지 클릭
-  $('.skill-img').click(function () {
-    let tabId = $(this).attr('data-tab');
-    $("#" + tabId).addClass('active').css('display', 'flex');
-    setTimeout(function () {
-      $('.skill-detail').css('background', 'white');
-      $('.detail-content, .detail-back').css('opacity', '1');
-      $('.skill-list>ul, .skill-title').css('opacity', '0')
-    }, 500);
-  });
-
-  // 스킬 뒤로가기 클릭
-  $('.detail-back').click(function () {
-    $('.skill-detail').fadeOut(500, function () {
-      setTimeout(function () {
-        $('.skill-detail').removeClass('active').css('display', 'none');
-        $('.skill-detail').css('background', '');
-        $('.detail-content, .detail-back').css('opacity', '0');
-      }, 300);
-    });
-    setTimeout(function () {
-      $('.skill-list>ul, .skill-title').css('opacity', '1');
-    }, 300);
-  });
-
-  // 프로젝트 화면 클릭
-  $(".sample-box").click(function () {
-    $(this).stop().fadeOut(200);
-  });
-
-  // 화면 드래그 이동
-  $("#project-icon, #skill-icon, .screen, #intro").draggable({
-    containment: "window"
-  });
-
-  // 공지 화면 클릭
-  $('#notice-close, .notice-box, .notice-icon').click(function () {
-    $('.notice-box').stop().fadeOut(400);
-    $('.notice>.notice-icon').delay(400).hide(100);
-  });
-
+// 공지 클릭
+$('#notice-close, .notice-box, .notice-icon').click(function () {
+  $('.notice-box').stop().fadeOut(400);
+  $('.notice>.notice-icon').delay(400).hide(100);
 });
+
+// 실시간 시간 변경
+const currentTime = () => {
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let time = document.getElementById('f-time');
+
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (hours < 12) {
+    time.innerHTML = '오전 ' + hours + ':' + minutes;
+  } else {
+    if (hours == 12) {
+      time.innerHTML = '오후 ' + hours + ':' + minutes;
+    } else if (hours > 12) {
+      hours = hours - 12;
+      time.innerHTML = '오후 ' + hours + ':' + minutes;
+    }
+  }
+  setTimeout('currentTime()', 1000);
+}
+window.onload = currentTime;
